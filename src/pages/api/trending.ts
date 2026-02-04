1fb515185f6dcc9daca52e3c64b10500e6085053
zoom <zoom@MacBook-Pro-zoom-9.local>
2026-02-05 02:40:44 +0800

    fix: trending API fully compatible with Edge Runtime

--- src/pages/api/trending.ts
+++ src/pages/api/trending.ts
@@ -1,47 +1,24 @@
 export const runtime = 'edge';
-import type { NextApiRequest, NextApiResponse } from 'next';
-const axios = require('axios');
-import Cors from 'cors';
 
-const cors = Cors({
-  methods: ['GET'],
-  origin: process.env.NEXT_PUBLIC_DOMAIN,
-  optionsSuccessStatus: 200,
-});
-
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
+export default async function handler(req: Request) {
+  try {
+    const response = await fetch('https://www.tiktok.com/node/share/discover');
+    const json = await response.json();
+    return new Response(
+      JSON.stringify({ data: json?.body }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
       }
-
-      return resolve(result);
-    });
-  });
-}
-
-export default async function handler(_req: NextApiRequest, res: NextApiResponse<any>) {
-  // Get data from your database
-  await runMiddleware(_req, res, cors);
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: 'https://www.tiktok.com/node/share/discover',
-  };
-
-  let data = null;
-
-  await axios
-    .request(config)
-    .then((response: any) => {
-      data = response.data?.body;
-    })
-    .catch((error: any) => {
-      data = null;
-    });
-
-  res.status(200).json({
-    data,
-  });
+    );
+  } catch (error) {
+    const errorMessage = typeof error === 'object' && error !== null && 'message' in error ? (error as any).message : 'Failed to fetch';
+    return new Response(
+      JSON.stringify({ data: null, error: errorMessage }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
 }
